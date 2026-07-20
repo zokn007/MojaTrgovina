f22245522428d8545e0d835f64ef14d2f3325e15
zokn007 <gz4sbmypky@privaterelay.appleid.com>
2026-07-20 21:29:13 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_VERSION='1.12.2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./cloud-sync.js','./update-manager.js','./version.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./favicon-32.png','./logo-lidl.jpeg','./logo-hofer.jpeg','./logo-spar.png','./logo-mercator.png','./logo-dm.jpeg','./logo-tus.png'];
+const CACHE_VERSION='1.13.0';
+const ASSETS=['./','./index.html','./manifest.json','./cloud-sync.js','./update-manager.js','./version.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./logo-lidl.jpeg','./logo-hofer.jpeg','./logo-spar.png','./logo-mercator.png','./logo-dm.jpeg','./logo-tus.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE_VERSION).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_VERSION).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
